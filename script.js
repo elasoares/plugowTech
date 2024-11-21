9a82ca46a1a039f3673ba71fdef20a45a157dfcf
Elaine Tavares <elainetavares.dev@gmail.com>
2024-11-21 10:53:55 -0300

    animação nos elementos ao 'scrollar' a página

--- script.js
+++ script.js
@@ -1,4 +1,31 @@
+// ScrollReveal().reveal('.headline')
 
+const left = ScrollReveal({
+    origin: "left",
+    distance: "80px",
+    duration: 1100,
+    reset: true,
+})
+
+left.reveal(".headline")
+
+const right = ScrollReveal({
+    origin: "right",
+    distance: "80px",
+    duration: 1100,
+    reset: true,
+})
+
+right.reveal(".right")
+
+const bottom = ScrollReveal({
+    origin: "bottom",
+    distance: "80px",
+    duration: 1100,
+    reset: true,
+})
+
+bottom.reveal(".bottom")
 
 
 
